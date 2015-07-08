32009f211526d2c05b209ad18811884c42746b15
Thomas Roch <thomas.c.roch@gmail.com>
2015-07-08 13:05:03 +0100

    Bump react and react-router versions

--- _scripts/build.js
+++ _scripts/build.js
@@ -73,7 +73,7 @@ function renderApi(done) {
 };
 
 async.parallel([
-    renderPage('index.html', {home: true, router5Version: '0.1.0-rc.5'}),
+    renderPage('index.html', {home: true, router5Version: '0.1.0-rc.6'}),
     renderDoc('_docs', 'why-router5.md', {whyRouter5: true}),
     renderDoc('_docs', 'get-started.md', {getStarted: true}),
     renderDoc('_guides', 'configuring-routes.md', {confRoutes: true, docs: true}),
@@ -84,8 +84,8 @@ async.parallel([
     renderApi,
     renderDoc('_docs', 'with-react.md', {react: true, examples: true, scripts: [
         'https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/react.min.js',
-        '/assets/router5.0.1.0-rc.5.min.js',
-        '/assets/router5-react.0.1.0-alpha.5.min.js',
+        '/assets/router5.0.1.0-rc.6.min.js',
+        '/assets/router5-react.0.1.0-alpha.6.min.js',
         '/assets/router5-react-example.js'
     ]})
 ], function (err, res) {
