f4ca9f2b412d43ad47d8a3b9c14702eb96b639db
Thomas Roch <thomas.roch@fanduel.com>
2015-10-27 02:00:40 +0000

    Update title

--- _scripts/build.js
+++ _scripts/build.js
@@ -17,7 +17,7 @@ marked.setOptions({
 var env = nunjucks.configure(path.join(__dirname, '../'), {watch: false});
 
 var data = {
-    pageTitle: 'router5 | HTML5 router for reactive applications',
+    pageTitle: 'router5 | flexible and powerful routing solution for web applications',
     styleSheets: ['//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/styles/monokai.min.css', '/styles/main.css']
 };
 
